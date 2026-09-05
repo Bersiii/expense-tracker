eddd530df55201069d252c7b2ce9f08d123c18ca
Bersiii <madonna1621@gmail.com>
2026-09-05 16:02:56 +0300

    Remove unnecessary import statement from Header component

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <header  className="w-full px-6 pt-0 mb-2 fixed top-0 left-0 z-50">
+    <header className="w-full px-6 pt-0 mb-2 fixed top-0 left-0 z-50">
       <nav className="mx-auto flex h-[64px] max-w-[1200px] items-center justify-between rounded-2xl border border-gray-200 bg-#aaabb0 px-6 shadow-sm backdrop-blur-md">
         {/* Logo */}
         <div className="flex items-center gap-2">
